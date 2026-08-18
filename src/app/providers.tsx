98682f95c11df8b8892ae1a47f21c919a2ecd7d8
Oyead <eyadswailam7@gmail.com>
2026-08-18 23:05:38 +0300

    feat: add NextAuth SessionProvider to app providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -12,8 +13,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }));
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
-      <QueryClientProvider client={client}>{children}</QueryClientProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+        <QueryClientProvider client={client}>{children}</QueryClientProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
