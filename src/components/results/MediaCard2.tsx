0d18dd7c0ffa1c3997a1fc4bbb2c4f64329d7790
Oyead <eyadswailam7@gmail.com>
2026-08-18 23:09:47 +0300

    feat: add placeholder MediaCard2 and TestCard components

---
+++ src/components/results/MediaCard2.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function MediaCard2() {
+  return <div>MediaCard2</div>;
+}
---
+++ src/components/results/TestCard.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function TestCard() {
+  return <div>Test</div>;
+}
